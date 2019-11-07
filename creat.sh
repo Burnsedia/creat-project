caf852b7486f6a0d3f25f22116027596e58ddde9
burnsedia <bburnsed1995@gmail.com>
2019-11-07 04:40:09 -0500

    init commit

---
+++ creat.sh
@@ -0,0 +1,13 @@
+!#/bin/bash
+
+function creat(){
+cd
+cd dev/
+git init $1
+hub creat
+git remote add origin git@github.com:Burnsedia/$1.git
+touch README.md
+git add -A
+git commit -a -m "initial commit"
+git push -u origin master
+}
